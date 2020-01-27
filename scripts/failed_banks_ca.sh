47df6dbc4889876443b12ebaf472fd92b45bda58
nisakhan <nisakhan@umich.edu>
2020-01-27 01:38:04 -0800

    Add files via upload

--- scripts/failed_banks_ca.sh
+++ scripts/failed_banks_ca.sh
@@ -1,17 +1,4 @@
-import csv
-import urllib.request
-import codecs
-
-url = "https://www.fdic.gov/bank/individual/failed/banklist.csv"
-ftpstream = urllib.request.urlopen(url)
-csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
-
-x = 0
-with open('failed_banks_ca.csv', 'w', newline='') as file:
-  writer = csv.writer(file)
-  writer.writerow(["Bank Name", "City", "ST", "CERT", "Acquiring Institution", "Closing Date"])
-  for row in csvfile:
-    if row[2] == 'CA':
-      x += 1
-      writer.writerow(row)
-print("There are " + str(x) + " failed banks in CA")
+wget "https://www.fdic.gov/bank/individual/failed/banklist.csv"
+csvgrep --columns ST --match CA banklist.csv > failed_banks_ca.csv
+X="$(wc -l < failed_banks_ca.csv)"
+echo "There are $(($X-1)) failed banks in CA"
